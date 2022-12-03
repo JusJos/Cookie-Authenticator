ef64548b5205e1b72b6a49359e26347a46fce756
Justin Joseph <91304818+JusJos@users.noreply.github.com>
2022-12-03 10:12:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const { signinHandler, welcomeHandler, refreshHandler, logoutHandler } = require('./handlers')
+const http = require('http')
+const app = express()
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+app.post('/signin', signinHandler)
+app.get('/welcome', welcomeHandler)
+app.post('/refresh', refreshHandler)
+app.get('/logout', logoutHandler)
+const server = http.createServer(app)
+server.listen(8080,()=>{
+    console.log("Good to go JARZ");
+})
