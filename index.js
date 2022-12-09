7d0b6aaaae7eeab2a6d3b7b56e8a1970c5e6c46a
Justin Joseph <91304818+JusJos@users.noreply.github.com>
2022-12-09 16:08:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ app.post('/refresh', refreshHandler)
 app.get('/logout', logoutHandler)
 const server = http.createServer(app)
 server.listen(8080,()=>{
-    console.log("Good to go JARZ");
+    console.log("Good to Go JARZ");
 })
